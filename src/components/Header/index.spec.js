6d3c063f301f567086c1019d1e7255e40c88e264
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-05-23 18:27:23 -0300

    Temporary failing tests
    for test automatic badge generation

---
+++ src/components/Header/index.spec.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+import { Header } from '.'
+
+describe("Header", () => {
+  describe("Layout", () => {
+    it('has header of Cotizador de seguros', () => {
+      const {container} = render(<Header titulo="Cotizador de seguros"/>)
+      const header = container.querySelector('h1')
+      expect(header).toHaveTextContent('X')
+    })
+  })
+})
